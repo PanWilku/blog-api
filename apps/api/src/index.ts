886f6ba6819a56955f18b41620eae6d89b2ca12f
Marcin Wilk <marcin.wilk.503@gmail.com>
2025-09-27 16:53:58 +0900

    sigint change

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -51,3 +51,11 @@ process.on("SIGTERM", () => {
     process.exit(0);
   });
 });
+
+process.on("SIGINT", () => {
+  console.log("SIGINT received, shutting down gracefully");
+  server.close(() => {
+    console.log("Server closed");
+    process.exit(0);
+  });
+});
